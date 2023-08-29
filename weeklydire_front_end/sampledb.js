ae0e91e8e79a810cc87a649f8a9dc2316335d393
Andrew Taylor <drew.j.taylor1@gmail.com>
2023-08-28 20:46:08 -0400

    Added link for dockerizing

---
+++ weeklydire_front_end/sampledb.js
@@ -0,0 +1,36 @@
+export const comments = [
+    {
+        id: 1,
+        body: 'This is the body of the first test comment. Lorem ipsum ad infinitum.',
+        subscriber_author: 1,
+        article: 1
+    }
+];
+
+export const creators = [
+    {
+        id: 1,
+        first_name: "Geoffrey",
+        last_name: "Chaucer",
+        email: 'ImAlwaysWrite@example.com',
+        password: 'password',
+        bio: 'Geoffrey Chaucer was an English poet, author, and civil servant best known for "The Canerbury Tales". More recently, he has written columns for "The Weekly Dire" on topics ranging from modern gardening to rocket science.',
+        is_admin: false
+    }
+];
+
+export const subscribers = [
+    {
+        id: 1,
+        email: 'ISubscribeToThings@example.com',
+        password: 'password'
+    }
+];
+
+export const articles = [
+    {
+        id: 1,
+        body: 'Lorem Ipsum articullum bodium. This is the test body of the first article.',
+        title: 'Test title'
+    }
+]
--- weeklydire_front_end/src/Components/Footer.js
+++ weeklydire_front_end/src/Components/Footer.js
@@ -1,13 +1,15 @@
-
+import {Container, Row, Col} from 'reactstrap'
 const Footer = () => {
   return (
     <div className="footer fixed-bottom">
-        <h1>This is the footer</h1>
-        <p>Link</p>
-        <p>Link</p>
-        <p>Link</p>
-        <p>Link</p>
-        <p>Trademark</p>
+        <Container>
+            <h1>This is the footer</h1>
+            <p>Link</p>
+            <p>Link</p>
+            <p>Link</p>
+            <p>Link</p>
+            <p>Trademark</p>
+        </Container>
     </div>
   )
 }

01f1f2dd68d14850509d2a04fb52d1279950c56b
Andrew Taylor <drew.j.taylor1@gmail.com>
2023-09-12 18:25:09 -0400

    Fixed footer  not touching bottom

--- weeklydire_front_end/src/Components/Footer.js
+++ weeklydire_front_end/src/Components/Footer.js
@@ -1,7 +1,7 @@
 import {Container, Row, Col} from 'reactstrap'
 const Footer = () => {
   return (
-    <div style={footerStyle} className='navbar-static-bottom'>
+    <footer style={footerStyle}>
         <Container>
             <h1>This is the footer</h1>
             <p>Link</p>
@@ -10,13 +10,15 @@ const Footer = () => {
             <p>Link</p>
             <p>Trademark</p>
         </Container>
-    </div>
+    </footer>
   )
 }
 
 const footerStyle = {
     backgroundColor: '#272727',
-    color: 'white'
+    color: 'white',
+    position: 'absolute',
+    width: '100%'
 }
 
 export default Footer

e62856423d3619cdd43cfe4702df10986ca17105
Andrew Taylor <drew.j.taylor1@gmail.com>
2023-09-17 22:37:52 -0400

    Added social buttons to bottom of page

--- weeklydire_front_end/src/Components/Footer.js
+++ weeklydire_front_end/src/Components/Footer.js
@@ -1,14 +1,25 @@
-import {Container, Row, Col} from 'reactstrap'
+import {Container, Row, Col} from 'reactstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faFacebookSquare, faInstagramSquare, faXTwitter} from '@fortawesome/free-brands-svg-icons';
+import gradientLogo from '../Assets/WeeklyDireLogoGradient.png';
+
 const Footer = () => {
   return (
     <footer style={footerStyle}>
         <Container>
-            <h1>This is the footer</h1>
-            <p>Link</p>
-            <p>Link</p>
-            <p>Link</p>
-            <p>Link</p>
-            <p>Trademark</p>
+            <Row>
+                <Col sm='3' className='text-center'>
+                    <h3>Social Media</h3>
+                    <h5>
+                        <FontAwesomeIcon icon={faFacebookSquare} style={{color: "#ffffff"}} />{'  '}
+                        <FontAwesomeIcon icon={faInstagramSquare} style={{color: "#ffffff"}} />{'  '}
+                        <FontAwesomeIcon icon={faXTwitter} style={{color: "#ffffff"}} />
+                    </h5>
+
+                </Col>
+                <Col sm='6' />
+                <Col sm='3'><img className='img-fluid logo' src={gradientLogo}  /></Col>
+            </Row>
         </Container>
     </footer>
   )

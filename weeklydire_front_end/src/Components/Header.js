fdcf022cf99c91eb38dee6cea22df2ab4c89463c
Andrew Taylor <drew.j.taylor1@gmail.com>
2023-09-17 21:49:55 -0400

    Added modal for future log in functionality

--- weeklydire_front_end/src/Components/Header.js
+++ weeklydire_front_end/src/Components/Header.js
@@ -1,13 +1,29 @@
-import {Container, Row, Col} from 'reactstrap';
+import {Container, Row, Col, Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 import gradientLogo from '../Assets/WeeklyDireLogoGradient.png';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 const Header = () => {
+
+    const [modal, setModal] = useState(false);
+    
+    const toggleModal = () => {
+        setModal(!modal)
+    }
+
   return (
     <Row style={headerStyle} className='sticky-top background-gray mb-3'>
+            <Col xs='1'></Col>
             <Col><Link to='/'><img className='img-fluid logo' src={gradientLogo}  /></Link></Col>
-            <Col>Sign in/sign up area</Col>
+            <Col xs='1'><Button onClick={toggleModal}>Sign in or Register</Button></Col>
+            <Col xs='1'></Col>
+            <Modal isOpen={modal} toggle={toggleModal}>
+                <ModalHeader toggle={toggleModal}>This will be the sign in area</ModalHeader>
+                <ModalBody>
+                    ModalBody
+                </ModalBody>
+            </Modal>
     </Row>
   )
 }

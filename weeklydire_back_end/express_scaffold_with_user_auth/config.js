ec3b917061ea11b56acd225c024c149a9e28c7c3
Andrew Taylor <drew.j.taylor1@gmail.com>
2023-10-17 15:09:34 -0400

    Updated db name

--- weeklydire_back_end/express_scaffold_with_user_auth/config.js
+++ weeklydire_back_end/express_scaffold_with_user_auth/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'mongoUrl' : 'mongodb://localhost:27017/startupdb',
-    'secretKey': 'dc4ejqc344fdasdfasdf3fh43nfqvjn23rf2309j1fad',
-    'mongoDbName': 'startupdb'
+    'secretKey': 'dc4ejqc344fdasdfasdf3fh43nzzfqv4jn23rf2309j1fad',
+    'mongoDbName': 'weeklydiredb'
 }

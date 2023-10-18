2aeb5ae81d17afeefd0d6f1e1b44320945531d90
Andrew Taylor <drew.j.taylor1@gmail.com>
2023-10-18 10:07:54 -0400

    basic GET/POST article routes

--- weeklydire_back_end/express_scaffold_with_user_auth/routes/articlesRouter.js
+++ weeklydire_back_end/express_scaffold_with_user_auth/routes/articlesRouter.js
@@ -6,7 +6,20 @@ const authenticate = require('../authenticate');
 const Article = require('../models/Article');
 
 articleRouter.route('/')
-.get()
-.post()
+.get((req, res, next) => {
+    Article.find()
+    .then(articles => {
+        if (!articles) {
+            return
+        };
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(articles)
+    })
+    .catch(err => next(err))
+})
+.post(authenticate.verifyUser, authenticate.verifyCreator, (req, res, next) => {
+    //Write logic to post article here
+})
 .put()
 .delete()

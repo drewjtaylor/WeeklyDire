bfe638d1d1cc787dc887a0328726ce1ea98c7605
Andrew Taylor <drew.j.taylor1@gmail.com>
2023-10-26 09:36:23 -0400

    Setting up mongodb

--- weeklydire_back_end/express_scaffold_with_user_auth/config.js
+++ weeklydire_back_end/express_scaffold_with_user_auth/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'mongoUrl' : 'mongodb://localhost:27017/startupdb',
+    'mongoUrl' : 'mongodb://localhost:27017/weeklydire',
     'secretKey': 'dc4ejqc344fdasdfasdf3fh43nzzfqv4jn23rf2309j1fad',
     'mongoDbName': 'weeklydiredb'
 }

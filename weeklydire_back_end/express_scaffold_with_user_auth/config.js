e3d706bbe4c6f47da91915be39cbf27c67042043
Andrew Taylor <drew.j.taylor1@gmail.com>
2023-10-26 11:43:21 -0400

    Actually connects to mongodb!

--- weeklydire_back_end/express_scaffold_with_user_auth/config.js
+++ weeklydire_back_end/express_scaffold_with_user_auth/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'mongoUrl' : 'mongodb://localhost:27017/weeklydire',
+    'mongoUrl' : 'mongodb://127.0.0.1:27017/weeklydire',
     'secretKey': 'dc4ejqc344fdasdfasdf3fh43nzzfqv4jn23rf2309j1fad',
     'mongoDbName': 'weeklydiredb'
 }

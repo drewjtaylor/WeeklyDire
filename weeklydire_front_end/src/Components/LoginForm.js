17972a953742b2b7a55823bb465a1fe92c88571b
Andrew Taylor <drew.j.taylor1@gmail.com>
2023-10-27 11:03:15 -0400

    Changed "username" to "email" on login form

--- weeklydire_front_end/src/Components/LoginForm.js
+++ weeklydire_front_end/src/Components/LoginForm.js
@@ -4,7 +4,7 @@ import {Label, Button, Container, Col, Row} from 'reactstrap';
 const LoginForm = () => {
 
     const initialValues = {
-        username: '',
+        email: '',
         password: '',
     };
 
@@ -14,7 +14,7 @@ const LoginForm = () => {
 <div>
     <Formik
       initialValues={{
-        username: '',
+        email: '',
         password: ''
       }}
       onSubmit={handleLoginSubmit}
@@ -23,10 +23,10 @@ const LoginForm = () => {
         <Form>
           <Row>
               <Col xs='3'>
-                <Label htmlFor="username" className='me-2'>Username:</Label>
+                <Label htmlFor="email" className='me-2'>Email:</Label>
             </Col>
               <Col xs='9'>
-                  <Field name="username" placeholder="" />
+                  <Field name="email" placeholder="" />
               </Col>
           </Row>
 

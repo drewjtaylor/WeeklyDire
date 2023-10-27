c3c3e095c60372cb4659b476b7f54d7369327ab2
Andrew Taylor <drew.j.taylor1@gmail.com>
2023-10-27 11:04:47 -0400

    Temporarily removed auth on comment/article routes

--- weeklydire_back_end/express_scaffold_with_user_auth/routes/articlesRouter.js
+++ weeklydire_back_end/express_scaffold_with_user_auth/routes/articlesRouter.js
@@ -22,22 +22,25 @@ articleRouter.route('/')
     .catch(err => next(err))
 })
 // Create a new article. Requires user to be a creator
-.post(authenticate.verifyUser, authenticate.verifyCreator, (req, res, next) => {
-    const {body, title, thumbnail, tags} = req.body;
-    if (body && title && thumbnail && tags) {
-        Article.create({body, title, thumbnail, tags, creator: req.user._id})
-        then(article => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(article)
-        })
-        .catch(err => next(err))
-    } else {
-        const err = new Error('This error came from "articlesRouter" in the back end')
-        err.statusCode = 400;
-        return next(err)
-    }
-})
+.post(
+    // authenticate.verifyUser, 
+    // authenticate.verifyCreator, 
+    (req, res, next) => {
+        const {body, title, thumbnail, tags} = req.body;
+        if (body && title && thumbnail && tags) {
+            Article.create({body, title, thumbnail, tags}) // After verifying user is working, add ", creator: req.user._id" after tags
+            .then(article => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(article)
+            })
+            .catch(err => next(err))
+        } else {
+            const err = new Error('This error came from "articlesRouter" in the back end')
+            err.statusCode = 400;
+            return next(err)
+        }
+    })
 .put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on articles. If you would like to edit an article, use "/articles/[articleId]" with a PUT request.')
--- weeklydire_back_end/express_scaffold_with_user_auth/routes/commentsRouter.js
+++ weeklydire_back_end/express_scaffold_with_user_auth/routes/commentsRouter.js
@@ -10,23 +10,10 @@ const User = require ('../models/User');
 
 
 commentRouter.route('/')
-.get((req, res) => {
+.all((req, res) => {
     res.statusCode = 403;
-    res.end('GET is not supported for /comments. Please use comments/[articleid]')
+    res.end('This operation is not supported for /comments. Please use comments/[articleid]')
 })
-.post((req, res) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /comments. Please use comments/[articleid]')
-})
-.put((req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /comments. If you want to edit a specific comment, Please use comments/[articleId]/[commentId]')
-})
-.delete((req, res) => {
-    res.statusCode = 403;
-    res.end('DELETE operation not supported on /comments')
-})
-
 
 commentRouter.route('/:articleId')
 // For getting all comments on a specific article with articleId
@@ -43,14 +30,14 @@ commentRouter.route('/:articleId')
 // For posting a new comment to article with articleId
 //  Must be a user. User posting is the 'author' for new comment
 .post(
-    authenticate.verifyUser, 
-    authenticate.verifyCreator, 
+    // authenticate.verifyUser, 
+    // authenticate.verifyCreator, 
     (req, res, next) => {
         const {body} = req.body;
 
         Comment.create({
             body: body,
-            author: req.user._id,
+            author: req.body.author, // Replace this line with "author: req.user._id, " after signing in users is figured out again
             article: req.params.articleId
         })
         .then(comment => {
@@ -64,12 +51,13 @@ commentRouter.route('/:articleId')
 
 // For editing an existing comment
 // Must be a user. User must match author
-.put(authenticate.verifyUser, (req, res, next) => {
+.put(
+    (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /comments/[articleId]. If you want to edit a specific comment, Please use comments/[articleId]/[commentId]')
 })
 // Must be a user. User must match author or be admin
-.delete(authenticate.verifyAdmin, (req, res, next) => {
+.delete((req, res, next) => {
     res.statusCode = 403;
     res.end('DELETE operation not supported on /comments/[articleId]. Please use comments/[articleId]/[commentId]')
 })

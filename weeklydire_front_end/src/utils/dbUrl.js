8d780298fbbf9659a109ac04bb9d2a7e828517d4
Andrew Taylor <drew.j.taylor1@gmail.com>
2023-12-01 11:16:39 -0500

    Change to Google Functions backend

--- weeklydire_front_end/src/utils/dbUrl.js
+++ weeklydire_front_end/src/utils/dbUrl.js
@@ -1,6 +1,6 @@
 // URL to back end when working locally
-export const dbUrl = 'http://localhost:3001' 
+// export const dbUrl = 'http://localhost:3001' 
 
 
 // URL to running google cloud function
-// export const dbUrl = 'https://us-central1-weeklydire.cloudfunctions.net/weeklydireexpress'
+export const dbUrl = 'https://us-central1-weeklydire.cloudfunctions.net/weeklydireexpress'

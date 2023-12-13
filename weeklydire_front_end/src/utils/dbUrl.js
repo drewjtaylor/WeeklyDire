184835a3a0b4696ac5acfcd50af3720f753efd63
Andrew Taylor <drew.j.taylor1@gmail.com>
2023-12-13 08:22:06 -0500

    Add note about switching back end connection

--- weeklydire_front_end/src/utils/dbUrl.js
+++ weeklydire_front_end/src/utils/dbUrl.js
@@ -1,3 +1,15 @@
+/** Database connections
+ *  To work on anything in the "back end folder" and see your changes, 
+ *  switch dbUrl to point at localhost.
+ * 
+ *  To make it work on Google Cloud again, upload the code to Google Cloud,
+ *  then switch it back to dbUrl points at their cloud function.
+ *  
+ *  If there's an error, something in the code probably didn't get uploaded.
+ */
+
+
+
 // URL to back end when working locally
 // export const dbUrl = 'http://localhost:3001' 
 

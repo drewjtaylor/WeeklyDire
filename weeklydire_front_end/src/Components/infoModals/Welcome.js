89fde5f113ecc3120d39b92ffadfeed03e86a8cf
Andrew Taylor <drew.j.taylor1@gmail.com>
2024-01-15 13:08:22 -0500

    Edit welcome modal for clarity

--- weeklydire_front_end/src/Components/infoModals/Welcome.js
+++ weeklydire_front_end/src/Components/infoModals/Welcome.js
@@ -5,20 +5,22 @@ const Welcome = ({modalStatus, modalToggle, toggleNextModal}) => {
       <Modal isOpen={modalStatus} toggle={modalToggle}>
   
           <ModalHeader toggle={modalToggle}>
-            Welcome!
+            Welcome to WeeklyDire!
           </ModalHeader>
   
           <ModalBody>
+          <p>WeeklyDire is a demonstration of my abilities to create an APP with the MERN stack, populated with real stories of optimisim and silver linings.</p>
+          
           <p>This is made of a MongoDB database, Express/Node.js for the server, and React.js for managing the interface.</p>
 
-          <p>Click below to see more details on these elements, or just click cancel to try using the site.</p>
+          <p>Click the blue "MongoDB" button below to start an overview of these elements, or just click cancel to try using the site.</p>
 
           <p>You can always click the "How was it made" button at the top to reopen this guide.</p>
           </ModalBody>
   
           <ModalFooter>
             <Button color="primary" onClick={() => {modalToggle(); toggleNextModal(true)}}>
-              See how MongoDB is used
+              MongoDB
             </Button>{' '}
             <Button color="secondary" onClick={modalToggle}>
               Close this guide

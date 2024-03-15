85423724f14c348497acb64b8121a23a37c8ad82
Andrew Taylor <drew.j.taylor1@gmail.com>
2024-03-15 16:57:24 -0400

    Use local express server

--- weeklydire_front_end/src/utils/dbUrl.js
+++ weeklydire_front_end/src/utils/dbUrl.js
@@ -2,7 +2,7 @@
  *  To work on anything in the "back end folder" and see your changes, 
  *  switch dbUrl to point at localhost.
  * 
- *  To make it work on Google Cloud again, upload the code to Google Cloud,
+ *  To make it work on Google Cloud again, upload the changes to Google Cloud,
  *  then switch it back to dbUrl points at their cloud function.
  *  
  *  If there's an error, something in the code probably didn't get uploaded.
@@ -11,8 +11,8 @@
 
 
 // URL to back end when working locally
-// export const dbUrl = 'http://localhost:3001' 
+export const dbUrl = 'http://localhost:3001' 
 
 
 // URL to running google cloud function
-export const dbUrl = 'https://us-central1-weeklydire.cloudfunctions.net/weeklydireexpress'
+// export const dbUrl = 'https://us-central1-weeklydire.cloudfunctions.net/weeklydireexpress'

1c59ae1611421f0832a08b93eb07eee0c3c23fe1
Andrew Taylor <drew.j.taylor1@gmail.com>
2024-06-19 11:38:26 -0400

    Add comments

--- weeklydire_front_end/src/Components/Logout.js
+++ weeklydire_front_end/src/Components/Logout.js
@@ -5,12 +5,15 @@ import { useContext } from "react";
 
 
 const Logout = () => {
+    // Get a function to remove cookie by name
+    // Example: removeCookie('jwt')
     // const [cookies, setCookie, removeCookie] = useCookies([]);
     const removeCookie = useCookies([])[2]
 
+    // Import the function to set the user from context
     // const [userFromContext, setUserFromContext] = useContext(UserContext);
     const setUserFromContext = useContext(UserContext)[1];
-    
+
     const handleLogout = () => {
         removeCookie('jwt');
         setUserFromContext({});

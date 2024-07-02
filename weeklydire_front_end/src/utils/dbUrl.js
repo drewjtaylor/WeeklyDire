49228e5a9237dc4fe6e1c7bf34e13192efefdd56
Andrew Taylor <drew.j.taylor1@gmail.com>
2024-07-01 20:33:15 -0400

    Update todos

--- weeklydire_front_end/src/utils/dbUrl.js
+++ weeklydire_front_end/src/utils/dbUrl.js
@@ -11,8 +11,8 @@
 
 
 // URL to back end when working locally
-// export const dbUrl = 'http://localhost:3001' 
+export const dbUrl = 'http://localhost:3001' 
 
 
 // URL to running google cloud function
-export const dbUrl = 'https://us-central1-weeklydire.cloudfunctions.net/weeklydireexpress'
+// export const dbUrl = 'https://us-central1-weeklydire.cloudfunctions.net/weeklydireexpress'
